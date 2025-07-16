21bb7172db076a43ab220d5d7adfc136ec1f335f
Steven Arellano <stevenjarellano2@gmail.com>
2025-07-16 19:51:40 +0000

    Add mi300x data.

--- benchmarks/Llama-4-Scout-17B-16E/4/MI300X/BF16/serve.sh
+++ benchmarks/Llama-4-Scout-17B-16E/4/MI300X/BF16/serve.sh
@@ -1,31 +1,20 @@
 set -euo pipefail
 
-<<<<<<< HEAD
-MODEL="meta-llama/Llama-3.1-70B-Instruct"
-TENSOR_PARALLEL_SIZE=4
-DTYPE="bfloat16"
-PORT="${PORT:-8004}"
-GPU_ID="${GPU_ID:-0,1,4,5}"
-=======
 MODEL="meta-llama/Llama-4-Scout-17B-16E"
 TENSOR_PARALLEL_SIZE=4
 DTYPE="bfloat16"
-PORT="${PORT:-8004}"
-GPU_ID="${GPU_ID:-4,5,6,7}"
->>>>>>> d1ca277 (Add mi300x benchmarks (some complete, some running).)
+PORT="${PORT:-8000}"
+GPU_ID="${GPU_ID:-0,1,2,3}"
 
 export CUDA_VISIBLE_DEVICES=$GPU_ID
 export HSA_VISIBLE_DEVICES=$GPU_ID
 export VLLM_USE_V1=1
 
-<<<<<<< HEAD
-=======
 # FOR THIS RUN
 export SAFETENSORS_FAST_GPU=1
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export VLLM_USE_MODELSCOPE=False
 
->>>>>>> d1ca277 (Add mi300x benchmarks (some complete, some running).)
 echo ">>> Starting vLLM server on GPU $GPU_ID (port $PORT) for model $MODEL"
 
 vllm serve "$MODEL" \
